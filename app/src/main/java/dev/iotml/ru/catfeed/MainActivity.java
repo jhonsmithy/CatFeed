ac347d40844c5912cf3b7f3bb7eafcb2122deee1
riverside <sylon@mail.ru>
2022-12-08 08:46:40 +0300

    first. wifi browser. manual ip settings

---
+++ app/src/main/java/dev/iotml/ru/catfeed/MainActivity.java
@@ -0,0 +1,87 @@
+package dev.iotml.ru.catfeed;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView = null;
+    String url_address;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        load_settings();
+
+        this.webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+        Toast.makeText(this, "http://"+url_address, Toast.LENGTH_SHORT).show();
+        webView.loadUrl("http://"+url_address);//192.168.217.103
+    }
+
+    private void load_settings() {
+        SharedPreferences stgs = getSharedPreferences("Settings.ini",MODE_PRIVATE);
+        url_address = stgs.getString("url_address","192.168.4.1");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings open", Toast.LENGTH_SHORT).show();
+            //Create new activity
+            Intent intent = new Intent(this, SettingsPage.class);
+            startActivityForResult(intent,1);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data==null){
+            return;
+        }
+        url_address=data.getStringExtra("name");
+        Toast.makeText(this, url_address, Toast.LENGTH_SHORT).show();
+        webView.loadUrl("http://"+url_address);//192.168.217.103
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/dev/iotml/ru/catfeed/SettingsPage.java
@@ -0,0 +1,48 @@
+package dev.iotml.ru.catfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsPage extends AppCompatActivity implements View.OnClickListener{
+    EditText url_edit;
+    Button button_save;
+    SharedPreferences sPref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
+        url_edit = (EditText) findViewById(R.id.url_edit);
+        button_save=(Button) findViewById(R.id.button_save);
+        button_save.setOnClickListener(this);
+        load_text();
+    }
+
+    private void load_text() {
+        sPref = getSharedPreferences("Settings.ini",MODE_PRIVATE);
+        String url_load_text = sPref.getString("url_address","192.168.4.1");
+        url_edit.setText(url_load_text);
+    }
+
+    @Override
+    public void onClick(View v) {
+        save_text();
+        Intent intent = new Intent();
+        intent.putExtra("name",url_edit.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+    private void save_text() {
+        sPref = getSharedPreferences("Settings.ini",MODE_PRIVATE);
+        SharedPreferences.Editor ed= sPref.edit();
+        ed.putString("url_address",url_edit.getText().toString());
+        ed.commit();
+    }
+
+}
